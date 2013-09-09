6aa7b0b5d21d75804f58899210947ab5a12abf3b
Chris Latko <clatko@playdom.com>
2013-09-09 10:34:18 -0700

    adding email templates

---
+++ lib/email/inner_html.php
@@ -0,0 +1,3 @@
+<div>
+	inner html
+</div>
---
+++ lib/email/template.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>template</title>
+</head>
+<body>
+
+
+<!-- CONTENT BEGIN -->
+<?= $html_content; ?>
+<!-- CONTENT END -->
+
+
+</body>
+</html>
